63205de63e3a1dc7bea607efbb4acbc88459e276
Daniil Galtsev <galtsev.daniil@gmail.com>
2021-04-30 09:56:33 +0300

    Added transforms. Fixed bug with val_transform. Fixed bug with HDF5Dataset.

--- parking_spot_detection/data/parking_spots/parking_spots.py
+++ parking_spot_detection/data/parking_spots/parking_spots.py
@@ -27,16 +27,26 @@ PROCESSED_DATA_DIRNAME = BaseDataModule.data_dirname() / "processed" / "parking_
 PROCESSED_DATA_FILENAME = PROCESSED_DATA_DIRNAME / "processed.h5"
 ESSENTIALS_FILENAME = PROCESSED_DATA_DIRNAME / "parking_dataset_essentials.json"
 
+REWRITE = False
 SEED = 0
 TRAINVAL_SPLIT = 0.9
 TRAIN_SPLIT = 0.95
 
-# Not yet calculated
-DATASET_MEAN = 0.0
-DATASET_STD = 1.0
+# Calculated using ..utils.calculate_mean_and_std with 4320 train images, seed=0.
+DATASET_MEAN = [0.4690, 0.4777, 0.4608]
+DATASET_STD = [0.2022, 0.1973, 0.2252]
+
+COLOR_JITTER = 0.0
+HORIZONTAL_FLIP = False
+DEGREES_AFFINE = 0
+TRANSLATE_AFFINE = 0.0
+SCALE_MARGIN_AFFINE = 0.0
+SHEAR_AFFINE = 0
+RANDOM_RESIZED_CROP = False
+RANDOM_ERASING = False
 
 USE_LOCAL = True
-WORKER_BUFFER_SIZE = 5000
+WORKER_BUFFER_SIZE = 5
 
 class ParkingSpots(BaseDataModule):
     """Parking Spots data module."""
@@ -49,9 +59,19 @@ class ParkingSpots(BaseDataModule):
         else:
             self.args = vars(args)
 
+        self.rewrite = self.args.get("rewrite", REWRITE)
         self.seed = self.args.get("seed", SEED)
         self.use_local = self.args.get("use_local", USE_LOCAL)
         self.worker_buffer_size = self.args.get("worker_buffer_size", WORKER_BUFFER_SIZE)
+
+        self.color_jitter = self.args.get("color_jitter", COLOR_JITTER)
+        self.horizontal_flip = self.args.get("horizontal_flip", HORIZONTAL_FLIP)
+        self.degrees_affine = self.args.get("degrees_affine", DEGREES_AFFINE)
+        self.translate_affine = self.args.get("translate_affine", TRANSLATE_AFFINE)
+        self.scale_margin_affine = self.args.get("scale_margin_affine", SCALE_MARGIN_AFFINE)
+        self.shear_affine = self.args.get("shear_affine", SHEAR_AFFINE)
+        self.random_resized_crop = self.args.get("random_resized_crop", RANDOM_RESIZED_CROP)
+        self.random_erasing = self.args.get("random_erasing", RANDOM_ERASING)
 
         with open(ESSENTIALS_FILENAME) as f:
             essentials = json.load(f)
@@ -70,7 +90,21 @@ class ParkingSpots(BaseDataModule):
         train_transform_list = [
             transforms.ToTensor()
         ]
+        if self.color_jitter:
+            train_transform_list.append(transforms.ColorJitter(self.color_jitter, self.color_jitter, self.color_jitter))
+        # transforms.RandomResizedCrop(size?, scale=(0.9, 1.0))
+        #TODO: add or remove RandomResizedCrop
+        train_transform_list.append(transforms.RandomAffine(
+            degrees=self.degrees_affine,
+            translate=(self.translate_affine, self.translate_affine),
+            scale=(1 - self.scale_margin_affine, 1 + self.scale_margin_affine),
+            shear=self.shear_affine
+        ))
+        if self.horizontal_flip:
+            train_transform_list.append(transforms.RandomHorizontalFlip())
         train_transform_list.append(transforms.Normalize(DATASET_MEAN, DATASET_STD))
+        if self.random_erasing:
+            train_transform_list.append(transforms.RandomErasing())
         self.train_transform = transforms.Compose(train_transform_list)
         self.val_transform = transforms.Compose([
             transforms.ToTensor(),
@@ -85,6 +119,10 @@ class ParkingSpots(BaseDataModule):
         """Adds argumets to parser required for Parking Spots Dataset."""
         parser = BaseDataModule.add_to_argparse(parser, main_parser)
         parser.add_argument(
+            "--rewrite", default=False, action="store_true",
+            help="If True, will processes raw data even if a processed file already exists."
+        )
+        parser.add_argument(
             "--use_local", default=False, action="store_true",
             help="If True, will use data from data folder, otherwise will download images from urls."
         )
@@ -92,12 +130,44 @@ class ParkingSpots(BaseDataModule):
             "--worker_buffer_size", type=int, default=WORKER_BUFFER_SIZE,
             help="A size of the buffer of each dataloader worker."
         )
+        parser.add_argument(
+            "--color_jitter", type=float, default=COLOR_JITTER,
+            help="Specifies the maximum random change in the brigtness, contranst and saturation of an image."
+        )
+        parser.add_argument(
+            "--horizontal_flip", default=False, action="store_true",
+            help="If True, will flip an image horizontally with p=0.5."
+        )
+        parser.add_argument(
+            "--degrees_affine", type=int, default=DEGREES_AFFINE,
+            help="A range (will be (-degrees_affine, +degrees_affine)) of degrees a random rotation is chosen from."
+        )
+        parser.add_argument(
+            "--translate_affine", type=float, default=TRANSLATE_AFFINE,
+            help="A maximum absolute fraction for translations."
+        )
+        parser.add_argument(
+            "--scale_margin_affine", type=float, default=SCALE_MARGIN_AFFINE,
+            help="Scaling factor margin (1 - scale_margin_affine < scale < 1 + scale_margin_affine)." 
+        )
+        parser.add_argument(
+            "--shear_affine", type=int, default=SHEAR_AFFINE,
+            help="A range (-shear_affine, +shear_affine) of degrees to select from."
+        )
+        parser.add_argument(
+            "--random_resized_crop", default=False, action="store_true",
+            help="If True, will perform random resize and rescaling before cropping to input size."
+        )
+        parser.add_argument(
+            "--random_erasing", default=False, action="store_true",
+            help="If True, will randomly erase pixels in a rectange region of an image."
+        )
         return parser
 
     def prepare_data(self) -> None:
         """Prepares data for the node globally."""
-        if not PROCESSED_DATA_FILENAME.exists():
-            _process_dataset(use_local=self.use_local)
+        if self.rewrite or not PROCESSED_DATA_FILENAME.exists():
+            _process_dataset(use_local=self.use_local, rewrite=self.rewrite, seed=self.seed)
         with ESSENTIALS_FILENAME.open("r") as f:
             self.essentials = json.load(f)
 
@@ -109,6 +179,7 @@ class ParkingSpots(BaseDataModule):
                 data_dataset_name="x_train",
                 targets_dataset_name="y_train",
                 worker_buffer_size=self.worker_buffer_size,
+                batch_size=self.batch_size,
                 transform=self.train_transform,
                 transform_target=None
             )
@@ -117,7 +188,8 @@ class ParkingSpots(BaseDataModule):
                 data_dataset_name="x_val",
                 targets_dataset_name="y_val",
                 worker_buffer_size=self.worker_buffer_size,
-                transform=self.val_transforms,
+                batch_size=self.batch_size,
+                transform=self.val_transform,
                 transform_target=None
             )
         if stage == "test" or stage is None:
@@ -126,6 +198,7 @@ class ParkingSpots(BaseDataModule):
                 data_dataset_name="x_test",
                 targets_dataset_name="y_test",
                 worker_buffer_size=self.worker_buffer_size,
+                batch_size=self.batch_size,
                 transform=self.val_transform,
                 transform_target=None
             )
@@ -198,11 +271,11 @@ def _process_dataset(
     PROCESSED_DATA_FILENAME.parent.mkdir(exist_ok=True, parents=True)
     with h5py.File(PROCESSED_DATA_FILENAME, "w") as f:
         dataset_train = f.create_dataset("x_train", shape=x_train_shape, chunks=chunk_shape, dtype=np.uint8)
-        f.create_dataset("y_train", data=y_train, dtype=np.int32)
+        f.create_dataset("y_train", data=y_train, dtype=np.int64)
         dataset_val = f.create_dataset("x_val", shape=x_val_shape, chunks=chunk_shape, dtype=np.uint8)
-        f.create_dataset("y_val", data=y_train, dtype=np.int32)
+        f.create_dataset("y_val", data=y_val, dtype=np.int64)
         dataset_test = f.create_dataset("x_test", shape=x_test_shape, chunks=chunk_shape, dtype=np.uint8)
-        f.create_dataset("y_test", data=y_train, dtype=np.int32)
+        f.create_dataset("y_test", data=y_test, dtype=np.int64)
 
         with mp.Pool(11) as pool:
             _process_split(pool, paths_train, dataset_train, resize_size, start_t, "train")
